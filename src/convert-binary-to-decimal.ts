d36a2eebc540726dbe85cb92ab9f3be45401ba04
Diogo-gallina <diogogallina20@gmail.com>
2023-06-22 23:00:05 -0300

    refactor: reviw-code

--- src/convert-binary-to-decimal.ts
+++ src/convert-binary-to-decimal.ts
@@ -19,39 +19,33 @@ const showAlert = (message: string) => {
 
 const convertBinaryToDecimal = (binaryString: string): number => {
     if (binaryString.trim() === '') {
-        showAlert('Insert a binary value, please.');
-        return 0;
+      showAlert('Insert a binary value, please.');
+      return 0;
     }
 
     if (!isValidBinary(binaryString)) {
-        showAlert('Entered characters can only be 0 or 1');
-        return 0;
+      showAlert('Entered characters can only be 0 or 1');
+      return 0;
     }
 
     const exponent = binaryString.length - 1;
   
-    const sum = binaryString.split('').reduce((accumulator, char, charNum) => {
-      const result = calculatePowerOfTwo(exponent - charNum);
+    const decimal = binaryString.split('').reduce((accumulator, char, charNum) => {
+      const result = Math.pow(2, exponent - charNum);
       const multipliedResult = multiplyResultByCharacter(result, char);
       return accumulator + multipliedResult;
     }, 0);
   
-    return sum;
+    return decimal;
   };
   
-  const calculatePowerOfTwo = (exponent: number): number => {
-    let result = 1;
   
-    for (let index = 0; index < exponent; index++) {
-      result *= 2;
-    }
-  
-    return result;
-  };
-  
-  const multiplyResultByCharacter = (result: number, char: string): number => {
-    return result * (Number(char) - 0);
-  };
+  const convertCharacterToNumber = (char: string) => {
+    return char === '1' ? 1 : 0;
+  }
+
+  const multiplyResultByCharacter = (result: number, char: string): number => result * convertCharacterToNumber(char);
+ 
   
 
 
